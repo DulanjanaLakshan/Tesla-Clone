621d5b08274d2863c36a4acfc4d9a91b498abe54
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-08-13 16:43:51 +0530

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,41 @@ import Section from './Section'
 const Home = () => {
   return (
     <div>
-      <Section/>
+      <Section
+        titel={'Model S'}
+        description={'Order Online for Touchless Delivery'}
+        backgroundImahe={'model-s.jpg'}
+        leftBtnText={'Customer Order'}
+        rightBtnText={'Existing inventory'}
+      />
+      <Section
+        titel={'Model Y'}
+        description={'Order Online for Touchless Delivery'}
+        backgroundImahe={'model-y.jpg'}
+        leftBtnText={'Customer Order'}
+        rightBtnText={'Existing inventory'}
+      />
+      <Section
+        titel={'Model X'}
+        description={'Order Online for Touchless Delivery'}
+        backgroundImahe={'model-x.jpg'}
+        leftBtnText={'Customer Order'}
+        rightBtnText={'Existing inventory'}
+      />
+      <Section
+        titel={'Model 3'}
+        description={'Order Online for Touchless Delivery'}
+        backgroundImahe={'model-3.jpg'}
+        leftBtnText={'Customer Order'}
+        rightBtnText={'Existing inventory'}
+      />
+      <Section
+        titel={'Lowest Cost Solar Panels in Amarica'}
+        description={'Money-back guarantee'}
+        backgroundImahe={'solar-panel.jpg'}
+        leftBtnText={'Order Now'}
+        rightBtnText={'Learn More'}
+      />
     </div>
   )
 }
